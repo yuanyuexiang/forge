e2127aaf33be5b4dbd96b09b6128620d9f9bd723
yuanyuexiang <yuanyuexiang@126.com>
2025-08-23 01:02:42 +0800

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 启用 Ant Design 的 styled-components SSR
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import StyledComponentsRegistry from './providers/AntdRegistry';
+import ApolloProvider from './providers/ApolloProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+
+        <StyledComponentsRegistry>
+          <ApolloProvider>
+            {children}
+          </ApolloProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
